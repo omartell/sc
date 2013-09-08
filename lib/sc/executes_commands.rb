94665dcb067e59d18043eb4162f32dea718996e6
Oliver Martell <oliver.martell@gmail.com>
2013-09-08 19:33:33 +0100

    Add class to show the track information

--- lib/sc/executes_commands.rb
+++ lib/sc/executes_commands.rb
@@ -9,6 +9,22 @@ class Sc::ExecutesCommands
   end
 end
 
+class Sc::TracksDisplayer
+  def show_matching_tracks(tracks)
+    tracks.each do |t|
+      puts "#{t.user.permalink}/#{t.permalink}"
+      puts "\t duration: #{miliseconds_to_minutes(t.duration)}"
+      puts "\t id: #{t.id}"
+    end
+  end
+
+  private
+
+  def miliseconds_to_minutes(duration)
+    Time.at(duration/1000).gmtime.strftime('%R:%S')
+  end
+end
+
 class Sc::SoundCloudClient
   def initialize(client)
     @client = client
