951b93bdca0833dc77d79b40d822c1d6a8032907
Oliver Martell <oliver.martell@gmail.com>
2013-09-29 23:36:24 +0100

    Small refactoring

--- lib/sc/executes_commands.rb
+++ lib/sc/executes_commands.rb
@@ -1,29 +1,30 @@
 class Sc::ExecutesCommands
+  attr_reader :player, :soundcloud_client
   def initialize(soundcloud_client, player)
     @soundcloud_client = soundcloud_client
     @player = player
   end
 
   def search(options, search_term, output)
-    tracks = @soundcloud_client.search_tracks(search_term)
+    tracks = soundcloud_client.search_tracks(search_term)
     output.show_matching_tracks(tracks)
   end
 
   def play_track_permalink(options, permalink, output)
-    track = @soundcloud_client.get_track_info_from_permalink(permalink)
-    @player.play_track(track)
+    track = soundcloud_client.get_track_info_from_permalink(permalink)
+    player.play_track(track)
     output.display_track_information(track)
   end
 
   def play_track_id(options, track_id, output)
-    track = @soundcloud_client.get_track_info_from_track_id(track_id)
-    @player.play_track(track)
+    track = soundcloud_client.get_track_info_from_track_id(track_id)
+    player.play_track(track)
     output.display_track_information(track)
   end
 
   def play_playlist_permalink(options, permalink, output)
-    playlist = @soundcloud_client.get_playlist_info_from_permalink(permalink)
-    @player.play_playlist(playlist)
+    playlist = soundcloud_client.get_playlist_info_from_permalink(permalink)
+    player.play_playlist(playlist)
     output.display_playlist_information(playlist)
   end
 end
@@ -63,7 +64,7 @@ class Sc::ConsoleLogger
 end
 
 class Sc::VLCPlayer
-
+  attr_reader :vlc_client
   def initialize(vlc_client)
     @vlc_client = vlc_client
   end
@@ -84,29 +85,35 @@ class Sc::VLCPlayer
 end
 
 class Sc::SoundCloudClient
+  attr_reader :client
   def initialize(client)
     @client = client
   end
 
   def search_tracks(query)
-    @client.get("/tracks", q: query)
+    client.get("/tracks", q: query)
   end
 
   def get_track_info_from_track_id(track_id)
-    @client.get("/tracks/#{track_id}")
+    client.get("/tracks/#{track_id}")
   end
 
   def get_playlist_info_from_permalink(permalink)
     if permalink.scan("sets").first
-      url = "http://soundcloud.com/#{permalink}"
+      resolve_permalink(permalink)
     else
       username, playlist = *permalink.scan(/(.*)\/(.*)/).first
-      url = "http://soundcloud.com/#{username}/sets/#{playlist}"
+      resolve_permalink("#{username}/sets/#{playlist}")
     end
-    @client.get("/resolve", url: url)
   end
 
   def get_track_info_from_permalink(permalink)
-    @client.get("/resolve", url: "http://soundcloud.com/#{permalink}")
+    resolve_permalink(permalink)
+  end
+
+  private
+
+  def resolve_permalink(permalink)
+    client.get("/resolve", url: "http://soundcloud.com/#{permalink}")
   end
 end
